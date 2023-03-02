d3be220189d3edd0b2ed17cdcd1d07abdb6add87
spol-29 <polineni.sriram9@gmail.com>
2023-03-01 22:55:12 -0600

    WRONG SOLUTION

--- 1083.cpp
+++ 1083.cpp
@@ -8,7 +8,7 @@ int main(){
     for(int i = 1; i < n; i++){
         for(int j = 1; j < n; j++){
             if(j == i){
-                continue;
+                continue;y
             }else if(j != i){
                 count++;
             }
